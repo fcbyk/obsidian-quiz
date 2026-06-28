d754de5a0cc3a23846abac0cd972177532dbe162
fcbyk <731240932@qq.com>
2026-06-28 22:02:03 +0800

    chore: rename plugin and add project documentation

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -10,7 +10,7 @@ if you want to view the source, please visit the github repository of this plugi
 `;
 
 const prod = process.argv[2] === 'production';
-const outDir = prod ? 'dist' : 'test/.obsidian/plugins/hello-plugin';
+const outDir = prod ? 'dist' : 'test/.obsidian/plugins/obsidian-mdq';
 
 const context = await esbuild.context({
 	banner: {
